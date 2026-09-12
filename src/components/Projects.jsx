740a605e6af514ecf7bcb62129fd6bc89adf2381
yassoelfar <yassermuhamed78@gmail.com>
2026-09-12 10:33:04 +0300

    add project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,11 +4,14 @@ import Astro from '../assets/Astro.jpeg'
 import FeedMe from '../assets/FeedMe.png'
 import BookStore from '../assets/BookStore.png'
 import Xora from '../assets/xora.svg'
+import Clinic_image from '../assets/Clinic.png'
 var signifyLink="https://www.linkedin.com/posts/yasser-muhamed-00b534234_graduationproject-signlanguage-machinelearning-activity-7349886752381120513-74Qf?utm_source=share&utm_medium=member_android&rcm=ACoAADp0igsBMb0bLzWBBnG6F5PNOFM4F-j8Wcs"
 var FeedMeLink="https://drive.google.com/file/d/1uCY4-qeLGFfxZn1vGTJbVn751M2htifG/view?usp=drivesdk"
 var BookStoreLink="https://drive.google.com/file/d/1uCseK29UpGRyQcq9rcsgN1Y2ajhpJI71/view?usp=drivesdk"
 var AstroLink="https://www.linkedin.com/feed/update/urn:li:activity:7353063090747277313/"
 var XoraLink = "https://yasserelfar.github.io/sass_landing_page/";
+var Clinic="https://lnkd.in/p/eyTRq9hg";
+var clinic_Github="https://github.com/yasserelfar/Clinic-Mangment.git";
 export default function Projects() {  
   return (
     <section
@@ -19,6 +22,14 @@ export default function Projects() {
         Projects
       </h2>
       <div className="relative z-10 flex flex-col items-center gap-14 w-full">
+        <ProjectsCard
+          image={Clinic_image}
+          title="Clinci Mangment System"
+          description="A full-stack ASP.NET Core app that takes a clinic from patient registration to diagnosis, with role-based dashboards for Admin, Doctor, and Reception, automatic doctor matching, and full patient history across every visit."
+          link={Clinic}
+          githubLink={clinic_Github}
+          tags={["ASP.NET Core"," Entity Framework", "PostgreSQL", " MVC Architecture Healthcare"]}
+        />
         <ProjectsCard
           image={signify}
           title="Signify"
